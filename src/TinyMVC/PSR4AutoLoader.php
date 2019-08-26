de94a3264839d55d7e452d06338d2a6f5fb1afa3
Jirayr Arzoumanian <jirayr@jva.am>
2019-08-27 01:27:25 +0200

    Refactor AutoLoader to Functional Programming

--- src/TinyMVC/PSR4AutoLoader.php
+++ src/TinyMVC/PSR4AutoLoader.php
@@ -5,29 +5,47 @@ namespace TinyMVC;
 class PSR4AutoLoader {
 
   private $baseDir;
+  private $frameworkDir;
 
-  public function __construct($baseDir){
-    $this->baseDir = $baseDir;
+  public function __construct($appDir, $frameworkDir = "TinyMVC") {
+    $this->appDir = $appDir;
+    $this->frameworkDir = $frameworkDir;
   }
 
   public function register() {
     spl_autoload_register(function($classname) {
-      $classnameParts = explode(CLASS_SEPARATOR, $classname);
-      if ($classnameParts[0] == "TinyMVC") {
-        $frameworkClassnameParts = array_slice($classnameParts, 1);
-        $frameworkClassname = implode(CLASS_SEPARATOR, $frameworkClassnameParts);
-        $classFilename = FRAMEWORK_PATH . str_replace(CLASS_SEPARATOR, DIRECTORY_SEPARATOR, $frameworkClassname) . '.php';
+      if ($this->isFrameworkClass($classname)) {
+        $this->loadFrameworkClass($classname);
       } else {
-        $classFilename = APP_PATH . str_replace(CLASS_SEPARATOR, DIRECTORY_SEPARATOR, $classname) . '.php';
-      }
-      if (file_exists($classFilename)) {
-        require_once $classFilename;
+        $this->loadAppClass($classname);
       }
     });
   }
 
+  private function isFrameworkClass($classname) {
+    $classnameParts = explode(CLASS_SEPARATOR, $classname);
+    return ($classnameParts[0] == $this->frameworkDir);
+  }
 
+  private function loadFrameworkClass($classname) {
+    $classnameParts = explode(CLASS_SEPARATOR, $classname);
+    $frameworkClassnameParts = array_slice($classnameParts, 1);
+    $frameworkClassname = implode(CLASS_SEPARATOR, $frameworkClassnameParts);
+    // TODO: use REGEX instead
+    $frameworkClassFilename = FRAMEWORK_PATH . str_replace(CLASS_SEPARATOR, DIRECTORY_SEPARATOR, $frameworkClassname) . '.php';
+    $this->loadClass($frameworkClassFilename);
+  }
 
+  private function loadAppClass($classname) {
+    $appClassFilename = APP_PATH . str_replace(CLASS_SEPARATOR, DIRECTORY_SEPARATOR, $classname) . '.php';
+    $this->loadClass($appClassFilename);
+  }
+
+  private function loadClass($classFilename) {
+    if (file_exists($classFilename)) {
+      require_once $classFilename;
+    }
+  }
 }
 
 ?>
